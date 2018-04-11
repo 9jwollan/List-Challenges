fa2d5de1a6062a472034ec0d3956486dfe1b1602
9jwollan <31480922+9jwollan@users.noreply.github.com>
2018-04-11 10:06:15 -0500

    Add files via upload

---
+++ listTasks (2).js
@@ -0,0 +1,79 @@
+//Task 1
+function largest(list){
+    let largetest = list[a];
+    for (let a = 0; a < list.length; a++){
+        if (max < list[a]){
+            max = list[a];
+        }
+    }
+    return largest;
+}
+
+//Task 2
+function reverse(list){
+
+}
+
+//Task 3
+function cotains(list,e){
+
+    for (let a = 0; a < list.length; a++){
+        if (list[a] == e){
+            return true;
+        }
+        //
+    }
+    return false;
+
+}
+
+//Task 4
+function oddElements(list){
+
+}
+
+//Task 5
+function total(list){
+    let t = 0;
+    for (var t = 0; t < list.length; t++) {
+        t = t+list[t];
+    }
+    return p;
+}
+}
+
+//Task 6
+function isPalindrome(list){
+
+}
+
+//Task 7
+//Do second to last
+function recursiveTotal(list){
+
+}
+//Task 8
+function concatenate(listA, listB){
+
+}
+
+//Task 9
+function weave(listA, listB){
+
+}
+
+//Task 10
+//Attempt last
+function mergeInOrder(listA, listB){
+
+}
+
+//Task 11
+function rotate(list,k){
+
+}
+
+//Task 12
+function fibonacci(){
+
+}
